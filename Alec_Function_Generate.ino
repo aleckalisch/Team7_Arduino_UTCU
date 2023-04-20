d9b00250db3dd7bce09af9549934c98aa2e30998
Alec Kalisch <aleck1@umbc.edu>
2023-04-20 09:45:31 -0400

    Drafted a Generate Function; Listed assumptions about the system are commented at the top of the file

---
+++ Alec_Function_Generate.ino
@@ -0,0 +1,32 @@
+//this file contains the generate function for the TCU
+// I am making a few assumptions that we might need to iron out, so bear with me
+// Assumption 1: Global variables are allowed in Arduinos
+// Assumption 2: We will be using Global Variables for the storage of function type as well as the actual data
+// Assumption 3: there is a global variable for the barker code that is preset and cannot be modified
+//function IDs were found in Annex 10 page 46
+#include <stdlib.h>
+int8_t function_Type_bin;
+char function_Type_name[] = "AZ"; //can also be EL, and BAZ
+int8_t barker_code = 0b11101;
+int8_t function_ID = 0b0000000;
+
+int8_t * generateData()
+{
+    int8_t data[10];
+    if(function_Type_name == "AZ")
+    {
+        int8_t AZ_function = 0b0011001; //all function ID's are 7 bits
+        data[0] = AZ_function; //stored in the first slot
+    }
+    else if(function_Type_name == "BAZ")
+    {
+        int8_t BAZ_function = 0b1001001; //all function ID's are 7 bits
+        data[0] = BAZ_function; //stored in the first slot
+    }
+    else if(function_Type_name == "EL")
+    {
+        int8_t EL_function = 0b1100001; //all function ID's are 7 bits
+        data[0] = EL_function; //stored in the first slot
+    }
+    return data;
+}
--- test.ino
+++
@@ -1,1 +0,0 @@
-//i am testing pushing to the repo
